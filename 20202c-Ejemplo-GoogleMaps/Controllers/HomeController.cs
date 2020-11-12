dbd5bbad18a79c53d2a794a2d6f7fa9c3b145ce5
Pablo Sanchez <psanchez@plus54.com>
2020-11-11 21:47:20 -0300

    agregando implementacion simple de marcadores en mapa de google

--- 20202c-Ejemplo-GoogleMaps/Controllers/HomeController.cs
+++ 20202c-Ejemplo-GoogleMaps/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _20202c_Ejemplo_GoogleMaps.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,16 @@ namespace _20202c_Ejemplo_GoogleMaps.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult VerMapa()
+        {
+            List<MarcadorGoogle> marcadores = new List<MarcadorGoogle>();
+            marcadores.Add(new MarcadorGoogle() { Titulo = "Buenos Aires", Lat = -34.6158037, Lng = -58.5033386 });
+            marcadores.Add(new MarcadorGoogle() { Titulo = "San Pablo", Lat = -34.8208575, Lng = -56.3765241 });
+            marcadores.Add(new MarcadorGoogle() { Titulo = "Montevideo", Lat = -23.6821604, Lng = -46.8754923 });
+            marcadores.Add(new MarcadorGoogle() { Titulo = "Bogota", Lat = 4.6482837, Lng = -74.247894 });
+
+            return View(marcadores);
         }
     }
 }
---
+++ 20202c-Ejemplo-GoogleMaps/Models/MarcadorGoogle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _20202c_Ejemplo_GoogleMaps.Models
+{
+    public class MarcadorGoogle
+    {
+        public string Titulo { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
